d1f2b0aa0ecc907b5e62ab92b79165d79bd6bdcb
Sam Alpren <samalpren@santavitaPRO.local>
2019-03-01 10:24:51 -0600

    first commit, adding index.html and main.js to repo

---
+++ main.js
@@ -0,0 +1,25 @@
+// These are all notes for myself
+
+// const firstName = "Sam";
+// const lastName = "Alpren";
+
+// function displayName () {
+//   // const firstName = "Samuel";
+//   console.log(`${firstName}`);
+// }
+
+
+// console.log(`${firstName}`);
+// // displayName();
+
+// displayName()
+// // displayName("Debra", "Gordon")
+// // displayName("Svetlana", "Irinov")
+// // displayName("Sequina", "Rodriguez")
+// // displayName("Jessawynne", "Parker")
+
+const addExcitement = () => {
+  console.log("This is a sentence.")
+}
+
+addExcitement();
